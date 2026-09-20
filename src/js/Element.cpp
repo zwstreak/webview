5cccd242d2828270d8e4ff782c9e3a69072faa4f
zwstreak <zwstreak@gmail.com>
2026-09-20 01:32:46 +0100

    i love macros oh wait i forgot to implement HTMLCollection oops

--- src/js/Element.cpp
+++ src/js/Element.cpp
@@ -5,19 +5,41 @@
 #include <format>
 #include <ranges>
 
-#define ATOM_FLAGS (JS_PROP_HAS_GET | JS_PROP_HAS_SET | JS_PROP_CONFIGURABLE | JS_PROP_WRITABLE)
-#define CREATE_PROPERTY(prop, obj) \
+#define ATOM_RW_FLAGS (JS_PROP_HAS_GET | JS_PROP_HAS_SET | JS_PROP_WRITABLE)
+#define ATOM_R_FLAGS (JS_PROP_HAS_GET)
+
+#define CREATE_ELEMENT() \
+	JS_NewObjectClass(ctx, JSEngine::element_id); \
+	JS_SetOpaque(element, node)
+
+#define CREATE_PROPERTY_RW(prop) \
 	JSAtom atom_##prop = JS_NewAtom(ctx, #prop); \
 	JSValue getter = JS_NewCFunction(ctx, get_##prop, "get " #prop, 1); \
 	JSValue setter = JS_NewCFunction(ctx, set_innerHTML, "set " #prop, 1); \
-	JS_DefineProperty(ctx, obj, atom_##prop, JS_UNDEFINED, getter, setter, ATOM_FLAGS); \
+	JS_DefineProperty(ctx, element, atom_##prop, JS_UNDEFINED, getter, setter, ATOM_RW_FLAGS); \
+	JS_FreeAtom(ctx, atom_##prop)
+
+#define CREATE_PROPERTY_R(prop) \
+	JSAtom atom_##prop = JS_NewAtom(ctx, #prop); \
+	JSValue getter = JS_NewCFunction(ctx, get_##prop, "get " #prop, 1); \
+	JS_DefineProperty(ctx, element, atom_##prop, JS_UNDEFINED, getter, JS_UNDEFINED, ATOM_R_FLAGS); \
 	JS_FreeAtom(ctx, atom_##prop)
 
 #define GET_NODE() static_cast<Node*>(JS_GetOpaque(this_val, JSEngine::element_id)); \
-	if (node == nullptr) { \
-		return JS_ThrowTypeError(ctx, "expected a node object"); \
-	}
+	if (node == nullptr) { return JS_ThrowTypeError(ctx, "expected a node object"); } \
+	JSEngine* engine = JSEngine::get(); \
+	if (engine == nullptr) { return JS_ThrowTypeError(ctx, "could not get the JS engine."); }
 
+// children //
+JSValue get_children(JS_PARAMS) {
+	Node* node = GET_NODE();
+	for (auto& childId : node->childrenNodes) {
+		Node* child = engine->nodes->get(childId);
+	}
+	return JS_ThrowTypeError(ctx, "not impl yet.");
+}
+
+// innerHTML //
 JSValue get_innerHTML(JS_PARAMS) {
 	Node* node = GET_NODE();
 	std::string html = stringifyHTMLChildren(node->childrenNodes);
@@ -44,9 +66,9 @@ JSValue set_innerHTML(JS_PARAMS) {
 }
 
 JSValue JS_NewElementFromNode(JSContext* ctx, Node* node) {
-	JSValue object = JS_NewObjectClass(ctx, JSEngine::element_id);
-	JS_SetOpaque(object, node);
-	CREATE_PROPERTY(innerHTML, object);
+	JSValue element = CREATE_ELEMENT();
+	CREATE_PROPERTY_RW(innerHTML);
+	CREATE_PROPERTY_R(children);
 
-	return object;
+	return element;
 }
